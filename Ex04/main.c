88dd8d48d50c7471dbbea17671f1c06b623547f0
Vinicius Marques <viniciusmarquesalpanhes34@gmail.com>
2025-03-05 10:30:40 -0300

    Commit Teste

--- Ex04/main.c
+++ Ex04/main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 
+//Estruturas de repetição, for, while, do..while
+//                         para, enquanto, faça...enquanto
+
+/*
+ Faça um programa, no qual receba e some 5 numeros
+ inteiros
+ *
+*/
+
+
 int main() {
-    printf("Hello, World!\n");
+
+    //Variaveis
+    int numero;
+    int soma = 0;
+
+    //Laço de repetição
+    for (int i = 0; i < 5; i++) {
+        //Entrada
+        printf("Informe um numero: ");
+         scanf("%d", &numero);
+
+        //Recebe os valores digitados pelo usuario e soma.
+        soma += numero;
+
+    }
+
+    //Saida
+    printf("A soma e %d", soma);
+
+
+
     return 0;
 }
