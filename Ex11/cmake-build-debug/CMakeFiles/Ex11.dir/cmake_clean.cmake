33dd762ec01e522fe25073bd236eac7cb21144bf
Vinicius Marques <viniciusmarquesalpanhes34@gmail.com>
2025-03-17 21:25:13 -0300

    commit teste

---
+++ Ex11/cmake-build-debug/CMakeFiles/Ex11.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ex11.dir/main.c.obj"
+  "CMakeFiles/Ex11.dir/main.c.obj.d"
+  "Ex11.exe"
+  "Ex11.exe.manifest"
+  "Ex11.pdb"
+  "libEx11.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Ex11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Ex11/cmake-build-debug/CMakeFiles/Ex11.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Ex11.
+# This may be replaced when dependencies are built.
