ee243180cd88cd4d5e40fc7800771d5d145c0bc0
Vinicius Marques <viniciusmarquesalpanhes34@gmail.com>
2025-03-18 10:35:04 -0300

    commit teste

--- cursoFuncoes/funcoes002/main.c
+++ cursoFuncoes/funcoes002/main.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
+/*
+Faça um programa que tenha uma função que,
+dado um caractere qualquer de entrada, retorne
+o mesmo caractere sempre em maiúsculo.
+ */
+
+char tudoMaisculo(char caractere);
 
 int main(void) {
-    printf("Hello, World!\n");
+
+    //Variaveis
+    char caractere = 'a';
+
+    //Variavel recebendo a funcao e passando o parametro
+    char maisculo  = tudoMaisculo(caractere);
+
+    //Exibe na tela
+    printf("O caractere %c maisculo e %c", caractere, maisculo);
+
     return 0;
 
-/*
- teste 02
- */
+
+}
+
+//Funcao
+char tudoMaisculo(char caractere) {
+
+    char maisculo = (int)caractere -32;
+
+    return maisculo;
 }
