07841f3569d0005d1f049b9b3f2b0ede859b829b
Vinicius Marques <viniciusmarquesalpanhes34@gmail.com>
2025-03-18 10:27:22 -0300

    Desafio 01

--- cursoFuncoes/funcoes001/main.c
+++ cursoFuncoes/funcoes001/main.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+/*
+Faça um programa que tenha uma função que receba um vetor de
+inteiros como parâmetro e retorne o maior valor.
+ */
+
+int recebeVetor(int vetor[], int tamanho);
+
+int main() {
+
+    int vetor[10] = {3, 3, 4, 8, 4, 9, 23, 78, 98, 99};
+
+    int maior = recebeVetor(vetor, 10);
+
+    printf("O maior numero armazenado no vetor foi: %d", maior);
+
     return 0;
 
-/*
- teste
- */
+}
+
+int recebeVetor(int vetor[], int tamanho) {
+
+    int maior = 0;
+
+    for (int i = 0; i < tamanho; i++) {
+        if (i == 0) {
+            maior = vetor[i];
+        }
+
+        if (vetor[i] > maior) {
+            maior = vetor[i];
+        }
+    }
+
+    return maior;
 }
