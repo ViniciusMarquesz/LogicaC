835b5c28c069f662fa66a165821ce5e4f4526c48
Vinicius Marques <viniciusmarquesalpanhes34@gmail.com>
2025-03-19 16:45:42 -0300

    Funções com ponteiro

---
+++ cursoPonteiros/ponteiros007/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void testeVariavel(int x);
+void testePonteiro(int *pX);
+
+
+int main(void) {
+
+    int teste = 1;
+    int *pTeste = &teste;
+
+    //
+    //testeVariavel(teste);
+    testePonteiro(pTeste);
+
+    printf("%i\n", teste);
+
+    getchar();
+    return 0;
+}
+
+void testeVariavel(int x) {
+    ++x;
+}
+
+void testePonteiro(int *pX) {
+    ++*pX;
+}
